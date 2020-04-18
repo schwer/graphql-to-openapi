d797f330d7e7a4462a79bfc1c1d780fb97d18e68
Peter Schwer <github@peterschwer.com>
2020-04-18 11:45:26 -0400

    Added text area styles.

---
+++ docs/src/textAreaStyles.ts
@@ -0,0 +1,9 @@
+import { CSSProperties } from "react";
+
+export const textAreaStyles: CSSProperties = {
+  fontFamily: 'Consolas, monospace, sans-serif',
+  fontSize: 11,
+  height: '100%',
+  resize: 'none',
+  width: '100%',
+};
