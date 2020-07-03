e1dd9c40480bdcb08d06c96fee783a5a8fd1cfd2
Peter Schwer <github@peterschwer.com>
2020-07-03 16:42:51 -0400

    prettiered.

--- jest.config.js
+++ jest.config.js
@@ -1,23 +1,16 @@
 module.exports = {
   globals: {
-    "ts-jest": {
-      tsConfig: "tsconfig.json"
-    }
+    'ts-jest': {
+      tsConfig: 'tsconfig.json',
+    },
   },
-  moduleFileExtensions: [
-    "ts",
-    "js"
-  ],
+  moduleFileExtensions: ['ts', 'js'],
   transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest"
+    '^.+\\.(ts|tsx)$': 'ts-jest',
   },
-  testMatch: [
-    "**/tests/**/*.ts"
-  ],
-  testPathIgnorePatterns: [
-    "/dist/"
-  ],
-  testEnvironment: "node",
+  testMatch: ['**/tests/**/*.ts'],
+  testPathIgnorePatterns: ['/dist/'],
+  testEnvironment: 'node',
   coverageThreshold: {
     global: {
       branches: 100,
