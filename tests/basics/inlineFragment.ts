b3489ce5e574684b133de02583ec9959d2d21283
Peter Schwer <688963+schwer@users.noreply.github.com>
2021-08-05 12:46:28 -0400

    issue-90 Added support for inline fragments. (#93)
    * Added support for inline fragments.
    * Added changelog.
    * Added reference to release plugin (updating the release behavior).

---
+++ tests/basics/inlineFragment.ts
@@ -0,0 +1,32 @@
+import { readFileSync } from 'fs';
+import * as path from 'path';
+import { graphqlToOpenApi } from '../../index';
+import * as assert from 'assert';
+import { stringify } from 'yaml';
+
+describe('inlineFragment', function () {
+  it('should produce a valid openapi spec', function () {
+    const schemaString = readFileSync(
+      path.join(__dirname, 'inlineFragmentSchema.graphql')
+    ).toString();
+    const inputQueryFilename = path.join(__dirname, 'inlineFragment.graphql');
+    const inputQuery = readFileSync(inputQueryFilename).toString();
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const expectedOutputYaml = readFileSync(
+      path.join(__dirname, 'inlineFragment.yaml')
+    ).toString();
+    const output = graphqlToOpenApi({
+      schemaString,
+      inputQuery,
+    });
+    const actualOutput = output.openApiSchema;
+    const actualOutputYaml = stringify(
+      JSON.parse(JSON.stringify(actualOutput)),
+      {
+        sortMapEntries: true,
+      }
+    );
+    assert.ok(!!actualOutputYaml);
+    assert.equal(actualOutputYaml, expectedOutputYaml);
+  });
+});
