ee69710ed20639c87a455367ac038658339a93cf
Nat Budin <nbudin@actbluetech.com>
2024-10-11 12:11:21 -0700

    Add a failing test

---
+++ tests/basics/fieldMerging.ts
@@ -0,0 +1,25 @@
+import { readFileSync } from 'fs';
+import * as path from 'path';
+import { graphqlToOpenApi } from '../../index';
+import * as assert from 'assert';
+import * as stringify from 'json-stable-stringify';
+
+describe('fieldMerging', function () {
+  it('should produce a valid openapi spec', function () {
+    const schema = readFileSync(
+      path.join(__dirname, 'fieldMergingSchema.graphql')
+    ).toString();
+    const inputQueryFilename = path.join(__dirname, 'fieldMerging.graphql');
+    const query = readFileSync(inputQueryFilename).toString();
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const expectedOutput = require('./fieldMerging.json');
+    const actualOutput = graphqlToOpenApi({
+      schema,
+      query,
+    }).openApiSchema;
+    const normalizedActualOutput = stringify(actualOutput, { space: '  ' });
+    const normalizedExpectedOutput = stringify(expectedOutput, { space: '  ' });
+    assert.ok(!!actualOutput);
+    assert.equal(normalizedActualOutput, normalizedExpectedOutput);
+  });
+});
