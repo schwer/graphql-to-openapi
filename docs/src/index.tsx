b753ffd287d31c8f4569efcd75fc78f26f665aba
Peter Schwer <688963+schwer@users.noreply.github.com>
2026-01-17 22:21:31 -0500

    Docs platform update.

--- docs/src/index.tsx
+++ docs/src/index.tsx
@@ -1,17 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { GraphQLToOpenApiDocs } from './GraphQLToOpenApiDocs';
-import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
   <React.StrictMode>
     <GraphQLToOpenApiDocs />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
